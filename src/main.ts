a48086cf84b15facae82e21b165762693a19d822
Souchan917 <h.so030502@gmail.com>
2026-02-11 10:40:00 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import Phaser from "phaser";
+
+new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 960,
+  height: 540,
+  backgroundColor: "#111111",
+  parent: "app",
+  scene: {
+    create() {
+      this.add.text(40, 40, "picto prototype", {
+        fontSize: "48px",
+        color: "#ffffff",
+      });
+    },
+  },
+});
